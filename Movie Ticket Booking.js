467adfd499076a1d99f11bdc724bf3572bc8cc6e
divyaharshitha1 <109957785+divyaharshitha1@users.noreply.github.com>
2023-10-24 11:47:25 +0530

    Add files via upload

---
+++ Movie Ticket Booking.js
@@ -0,0 +1,29 @@
+let seats = document.querySelector(".all-seats");
+for (var i = 0; i < 59; i++) {
+    let randint = Math.floor(Math.random() * 2);
+    let selected = randint === 1 ? "booked" : "";
+    seats.insertAdjacentHTML(
+        "beforeend",
+        '<input type="checkbox" name="tickets" id="s' + (i + 2) + '" /><label for="s' + (i + 2) + '" class="seat ' + selected + '"></label>'
+    );
+}
+
+let tickets = seats.querySelectorAll("input");
+tickets.forEach((ticket) => {
+    ticket.addEventListener("change", () => {
+        let amount = document.querySelector(".amount").innerHTML;
+        let count = document.querySelector(".count").innerHTML;
+        amount = Number(amount);
+        count = Number(count);
+
+        if (ticket.checked) {
+            count += 1;
+            amount += 200;
+        } else {
+            count -= 1;
+            amount -= 200;
+        }
+        document.querySelector(".amount").innerHTML = amount;
+        document.querySelector(".count").innerHTML = count;
+    });
+});
